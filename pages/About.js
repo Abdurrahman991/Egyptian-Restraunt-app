429b08e0113d8c6bc95641b113699380d2b2cebc
Abdurahman Saad <114738485+Abdurrahman991@users.noreply.github.com>
2023-02-27 11:57:19 +0300

    Update About.js

--- pages/About.js
+++ pages/About.js
@@ -22,7 +22,7 @@ function About() {
           error repudiandae fuga? Ipsa laudantium molestias eos sapiente
           officiis modi at sunt excepturi expedita sint? Sed quibusdam
           recusandae alias error harum maxime adipisci amet laborum.
-          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
+          erspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
           cumque velit
         </p>
       </div>
